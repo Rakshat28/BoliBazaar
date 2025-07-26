45827da165eca124af46be257b3acc59a29fbcf2
Yash <yashbhardwaj7890@gmail.com>
2025-07-26 22:53:43 +0530

    Remove MapSelectorClient component , which is no longer needed following recent UI updates.

--- src/app/MapSelectorClient.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import dynamic from 'next/dynamic';
-import React from 'react';
-
-const MapSelectorWithNoSSR = dynamic(() => import('@/components/MapSelector'), { ssr: false });
-
-export default function MapSelectorClient() {
-  return <MapSelectorWithNoSSR />;
-} 
