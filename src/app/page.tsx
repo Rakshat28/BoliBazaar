e06ff41e5d7bf81194a9abf4ffdd165fb77bdaaf
Rakshat <rakshat07ps@gmail.com>
2025-07-26 14:40:48 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,10 @@ import SolutionSection from "@/components/SolutionSection";
 import ImpactSection from "@/components/ImpactSection";
 import CTASection from "@/components/CTASection";
 import Footer from "@/components/Footer";
-import Navbar from "@/components/Navbar";
 import React from 'react';
 const Index = () => {
   return (
     <div className="min-h-screen">
-      <Navbar />
       <HeroSection />
       <ProblemSection />
       <SolutionSection />
